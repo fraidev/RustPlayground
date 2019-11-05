31f502d0c1602c7516e435dc35ad3e843c019082
Frai <fraifelipe@gmail.com>
2019-11-04 23:57:42 -0300

    struct

---
+++ src/animal.rs
@@ -0,0 +1,4 @@
+pub struct Animal {
+    pub name: String,
+    pub age: u8,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+mod animal;
+use animal::Animal;
+
 fn main() {
     let x = 42;
 
+    let thor = Animal {
+        name: String::from("Thor"),
+        age: 0,
+    };
 
-    println!("Hello, world!: {}", x);
+
+    println!("His name is: {}", thor.name);
+    println!("And he is {} years old", thor.name);
 }
